797dd2fd5c1c42ecc3116c919f8cd5c535381fa6
takeshita-0x0201 <takeshita.0x0201@gmail.com>
2025-07-04 17:16:17 +0900

    Update main.py

--- src/web/main.py
+++ src/web/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://localhost:3000"],
+    allow_origins=["http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
